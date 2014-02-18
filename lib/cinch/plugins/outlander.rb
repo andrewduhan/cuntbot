33989eb8661b6919e15a75e0c5981d088274eb90
Andrew Duhan <andrewduhan@gmail.com>
2014-02-17 21:39:50 -0600

    outlander plugin responds to bot name

--- lib/cinch/plugins/outlander.rb
+++ lib/cinch/plugins/outlander.rb
@@ -13,23 +13,29 @@ module Cinch::Plugins
     end
 
     set :prefix, //
-    match /outlander/
     listen_to :channel
 
-    def execute(m)
+    def burp(m)
       message_word_length = m.message.scan(' ').length
       max_reply_length = message_word_length > 40 ? 40 : message_word_length
       max_reply_length = 10 if max_reply_length < 10
       max_reply_length = max_reply_length * 2
-      response = @markov.generate_n_words rand(10..max_reply_length)
+      response = (@markov.generate_n_words rand(10..max_reply_length)).split(' ').each { |word|
+        random_number = rand(0..100)
+        if random_number > 80
+          word.upcase!
+        elsif random_number < 20
+          word.downcase!
+        end
+      }.join(' ')
       sleep(rand(2..6))
       m.reply response
     end
 
     def listen(m)
-      if @timer + 60 < Time.now && rand(0..100) > 99
+      if (@timer + 60 < Time.now && rand(0..100) > 99) || (m.message =~ /#{@bot.nick}/)
         @timer = Time.now
-        execute(m)
+        burp(m)
       end
     end
   end
